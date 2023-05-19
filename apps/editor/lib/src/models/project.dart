702e07b2d9600c4a3d6e08e920a813431c8aaf1a
ampatspell <ampatspell@gmail.com>
2023-05-19 17:11:58 +0300

    Update project.dart

--- apps/editor/lib/src/models/project.dart
+++ apps/editor/lib/src/models/project.dart
@@ -12,18 +12,7 @@ class ProjectModel with _$ProjectModel implements HasDoc {
 
   const ProjectModel._();
 
-  String get name => doc['name'] as String; // ? ?? 'Untitled';
-
-  String? get node => doc['node'] as String?;
-
-  int get pixel => doc['pixel'] as int? ?? 2;
-
-  // TODO: per workspace
-  Future<void> updateNodeId(String? id) async {
-    await doc.merge({
-      'node': id,
-    });
-  }
+  String get name => doc['name'] as String;
 
   Future<void> delete() async {
     await doc.delete();
@@ -38,19 +27,21 @@ class ProjectStateModel with _$ProjectStateModel implements HasDoc {
 
   const ProjectStateModel._();
 
-  String? get workspace => doc['workspace_id'] as String?;
-
-  String? get sidebarTab => doc['sidebar_tab'] as String?;
-
-  Future<void> updateSidebarTab(String? value) async {
-    await doc.merge({
-      'sidebar_tab': value,
-    });
-  }
-
-  Future<void> updateWorkspaceId(String? id) async {
-    await doc.merge({
-      'workspace_id': id,
-    });
-  }
+// int get pixel => doc['pixel'] as int? ?? 2;
+//
+// String? get workspace => doc['workspace_id'] as String?;
+//
+// String? get sidebarTab => doc['sidebar_tab'] as String?;
+//
+// Future<void> updateSidebarTab(String? value) async {
+//   await doc.merge({
+//     'sidebar_tab': value,
+//   });
+// }
+//
+// Future<void> updateWorkspaceId(String? id) async {
+//   await doc.merge({
+//     'workspace_id': id,
+//   });
+// }
 }
